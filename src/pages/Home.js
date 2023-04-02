1b5fc11291c598dc0b1c5c1b3978ec816c9f4953
JinGee <jingee.kim9@gmail.com>
2023-04-02 13:37:01 +0900

    add backend to signup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -54,7 +54,7 @@ const db = getFirestore(app);
 
 export default function Home() {
 
-    const [isLoggedin, setIsLoggedIn] = useState(false);
+    const [isLoggedin, setIsLoggedIn] = useState(true);
 
     const [backgroundOption, setBackgroundOption] = useState("change_bg_option_1");
     const [color1, setColor1] = useState("#efefef");
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -8,7 +8,22 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { message, Space } from 'antd';
 import 'antd/dist/antd.css';
 import { encryptstorage } from '../components/encrypt'
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBDh3yxYRLCaikdnMXYrCuVc0iGL5qn0js",
+    authDomain: "dashboard-2a1a3.firebaseapp.com",
+    projectId: "dashboard-2a1a3",
+    storageBucket: "dashboard-2a1a3.appspot.com",
+    messagingSenderId: "354314041590",
+    appId: "1:354314041590:web:32b771d8e2a2d4ce4ad4d7",
+    measurementId: "G-W02KFP0FY8"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 
 export default function Signup(props){
@@ -32,7 +47,7 @@ export default function Signup(props){
         }
     }, [])
 
-    const login = () => {
+    const register = () => {
 
         if(username == ""){
             message.error("Please input a username");
@@ -52,13 +67,38 @@ export default function Signup(props){
 
         setIsLoading(true);
         
+        createUserWithEmailAndPassword(auth, username, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            setIsLoading(false);
+            navigate("/Home", {replace: true});
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            if(errorCode == "auth/invalid-email")
+            {
+                message.error("Invalid email format.");
+                setIsLoading(false);
+            }
+            else if(errorCode == 'auth/email-already-in-use')
+            {
+                message.error("Account already exists.");
+                setIsLoading(false);
+            }
+            else if(errorCode == 'auth/weak-password')
+            {
+                message.error("Password is too weak.");
+                setIsLoading(false);
+            }
+        })
 
 
     }
 
     const enterPress = (el) => {
         if (el.keyCode === 13){
-            login();
+            register();
         }
     }
 
@@ -93,7 +133,7 @@ export default function Signup(props){
                                 <input type="password" className="form-control" id="inputPassword" aria-describedby="passwordHelp" placeholder="Enter your password again" onChange={(event) => setPassword2(event.target.value)} onKeyDown={(el) => enterPress(el)}/>
                                 <small id="usernameHelp" className="form-text text-muted">Enter your new password again</small>
                             </div>
-                            <button type="submit" onClick={() => {login();}} className={`${s.login_button} btn btn-primary shadow-none`} style={{width: '100%', marginTop: 20}}>
+                            <button type="submit" onClick={() => {register();}} className={`${s.login_button} btn btn-primary shadow-none`} style={{width: '100%', marginTop: 20}}>
                                     { 
                                     isLoading 
                                     ? 
